8562dfddf872ccb777637e3651707ceb79bd3039
Dan Mallinger <589101+dmallinger@users.noreply.github.com>
2019-08-05 16:01:21 -0400

    adding commments and documentation

--- apps/tasks/utils.py
+++ apps/tasks/utils.py
@@ -7,7 +7,12 @@ import settings
 
 
 def require_cron_job(function):
-    """DECORATOR"""
+    """DECORATOR function.  Only allow function to be run by
+    App Engine cron.
+
+    :param function: The view to be run as a cron job.
+    :return: The decorated function
+    """
     @wraps(function)
     def decorated_function(*args, **kwargs):
         if request.headers.get("X-Appengine-Cron") == "true":
@@ -19,7 +24,12 @@ def require_cron_job(function):
 
 
 def require_task_api_key(function):
-    """DECORATOR"""
+    """DECORATOR function.  Determines if the task API key is including
+    via POST data.  If not, aborts.
+
+    :param function: the view function being decorated
+    :return: The decorated function
+    """
     @wraps(function)
     def decorated_function(*args, **kwargs):
         if request.form.get("TASK_API_KEY") == settings.TASK_API_KEY:
@@ -30,11 +40,12 @@ def require_task_api_key(function):
 
 
 def add_task(relative_uri, form_data=None):
-    """
+    """Submits a task for execution.  Includes a task API key by default
+    for security.  This key must be checked for by the task being run!
 
-    :param relative_uri:
-    :param form_data:
-    :return:
+    :param relative_uri: Task URI
+    :param form_data: Data to be passed to task
+    :return: Task response object
     """
     if form_data is None:
         form_data = {}
